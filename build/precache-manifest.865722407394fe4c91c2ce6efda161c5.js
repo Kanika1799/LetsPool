086a1afe631c4137949b8e995e7226d0c2447ce8
Kanika1799 <kanika17agrawal99@gmail.com>
2020-05-28 02:56:29 +0530

    Final

---
+++ build/precache-manifest.865722407394fe4c91c2ce6efda161c5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626c6ce6b15d3f2f396944e024bba044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a34df623a02b658e9f",
+    "url": "/static/css/2.f7e6c76a.chunk.css"
+  },
+  {
+    "revision": "437ed8effb4e9dd29e24",
+    "url": "/static/css/main.5042a130.chunk.css"
+  },
+  {
+    "revision": "a5a34df623a02b658e9f",
+    "url": "/static/js/2.974c3f0e.chunk.js"
+  },
+  {
+    "revision": "5b4c524ff1ac7e6259e532eb797f0d3e",
+    "url": "/static/js/2.974c3f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437ed8effb4e9dd29e24",
+    "url": "/static/js/main.c4aa29d8.chunk.js"
+  },
+  {
+    "revision": "f1de2e8472e1f1903a21",
+    "url": "/static/js/runtime-main.ad59dc14.js"
+  },
+  {
+    "revision": "66a0ceb4acc0ff8b2c7fa9405f231dcd",
+    "url": "/static/media/Buy1.66a0ceb4.png"
+  },
+  {
+    "revision": "1e8e76a63c9b60c249f81a5f6508264b",
+    "url": "/static/media/Buy2.1e8e76a6.png"
+  },
+  {
+    "revision": "be327a5b768d47a318b5e7ed7f622fc0",
+    "url": "/static/media/Buy3.be327a5b.png"
+  },
+  {
+    "revision": "c661a5fe26f7e2baca312cd5126c7b10",
+    "url": "/static/media/Pool1.c661a5fe.png"
+  },
+  {
+    "revision": "bed812b6d5344908ed6c7e3e7feeebba",
+    "url": "/static/media/Pool2.bed812b6.png"
+  },
+  {
+    "revision": "921199bea2114ae64b36766b704dd940",
+    "url": "/static/media/PoolSymbol.921199be.svg"
+  },
+  {
+    "revision": "aba7f6d14eb7d0b1fece4476a995aaa2",
+    "url": "/static/media/Step1.aba7f6d1.png"
+  },
+  {
+    "revision": "68c971593a4f3d2775c16f36f74c87cc",
+    "url": "/static/media/Step2.68c97159.png"
+  },
+  {
+    "revision": "4cb87c4ed76cb1e9164cbd3d4dcb838c",
+    "url": "/static/media/Step3.4cb87c4e.png"
+  },
+  {
+    "revision": "7204a1d21c30cc3790971a79db75c9f1",
+    "url": "/static/media/Step4.7204a1d2.png"
+  },
+  {
+    "revision": "c40b6cb01cdeca1443241abfd22d2546",
+    "url": "/static/media/Two.c40b6cb0.png"
+  },
+  {
+    "revision": "eaec0dc58a683fb05c22fd8e8abcd7c9",
+    "url": "/static/media/four.eaec0dc5.png"
+  },
+  {
+    "revision": "48d059e784f517c88b43e2356a494b82",
+    "url": "/static/media/pooltogether.48d059e7.png"
+  }
+]);
